a5d5dce108880a9f22c5d269dc614135f3035cac
R Akash <akashrangesh07@gmail.com>
2024-03-25 13:54:05 +0530

    refactored sheet page

--- src/app/sheet/page.tsx
+++ src/app/sheet/page.tsx
@@ -1,5 +1,11 @@
+'use client';
+import Navbar from '@/components/Navbar';
 import { SheetProps } from '../../../types/types';
-import SheetClient from './sheet-client';
+import { Card } from '@/components/Card';
+import { useState } from 'react';
+import { Ubuntu } from 'next/font/google';
+
+const ubuntu = Ubuntu({ weight: '500', subsets: ['latin'] });
 
 export default function Sheet({
   session_user,
@@ -7,12 +13,35 @@ export default function Sheet({
   problems,
   current_user_id,
 }: SheetProps) {
+  const [total_solved, setTotal_Solved] = useState(
+    attempted_problems.reduce((acc, pb) => {
+      if (pb.status === 'SOLVED') {
+        return acc + 1;
+      }
+      return acc;
+    }, 0)
+  );
   return (
-    <SheetClient
-      session_user={session_user}
-      current_user_id={current_user_id}
-      attempted_problems={attempted_problems}
-      problems={problems}
-    />
+    <div className="w-full min-h-screen">
+      <Navbar
+        session_user={session_user}
+        attempted_problems={attempted_problems}
+        total_solved={total_solved}
+      />
+      <div className={`lg:md:w-[70%] w-full m-auto ${ubuntu.className}`}>
+        <div className="grid grid-cols-1 gap-10 p-10">
+          {problems.map((problem, i) => (
+            <div key={i} id={problem.topic}>
+              <Card
+                topic={problem.topic}
+                problem_names={problem.names}
+                user_id={current_user_id}
+                onStatusUpdate={setTotal_Solved}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
