c907615050f5ec4763a4d46cce3aa52f89279c8a
R Akash <akashrangesh07@gmail.com>
2024-03-30 15:21:31 +0530

    fixed logout dropdown and added router for firing logout

--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { Ubuntu } from 'next/font/google';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 const ubuntu = Ubuntu({ weight: '500', subsets: ['latin'] });
@@ -11,27 +12,25 @@ export default function ProfilePicture({
   profile_picture: string;
 }) {
   const [isOpen, setIsOpen] = useState(false);
-
+  const router = useRouter();
   return (
-    <div className="">
+    <div className="relative">
       <Image
         src={profile_picture}
         alt="profile_picture"
         width={50}
         height={50}
-        className="rounded-full relative hover:opacity-70"
+        className={`rounded-full  hover:opacity-70 ${isOpen && 'opacity-70'}`}
         onClick={() => setIsOpen(!isOpen)}
       />
       {isOpen && (
         <div
-          className="absolute lg:top-28 top-32 lg:md:right-[20.2rem] right-[1.7rem] bg-white p-2 flex rounded-md hover:cursor-pointer
+          className="absolute top-16 -right-3 bg-white p-2 flex rounded-md hover:cursor-pointer
          hover:bg-slate-100"
         >
           <p
             className={`text-red-700 ${ubuntu.className}`}
-            onClick={() =>
-              (window.location.href = 'http://localhost:3000/api/auth/logout')
-            }
+            onClick={() => router.push('/api/auth/logout')}
           >
             Logout
           </p>
